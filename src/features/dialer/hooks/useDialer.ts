8734e3e61666f8236431f5c107187fdc1052e195
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-09-02 18:52:00 +0530

    clientRef.current.userId

--- src/features/dialer/hooks/useDialer.ts
+++ src/features/dialer/hooks/useDialer.ts
@@ -118,7 +118,7 @@ export const useDialer = ({ clientRef }: UseDialerProps) => {
 
       const call = new StringeeCall(
         clientRef.current,
-        clientRef.current. || "agent",
+        clientRef.current.userId || "agent",
         cleanNumber,
         false
       );
